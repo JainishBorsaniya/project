1d2957975d5e04d0f787944476c7d98c30198415
JainishBorsaniya <71925738+JainishBorsaniya@users.noreply.github.com>
2022-03-28 12:09:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+const form = document.querySelector('#searchForm');
+const res = document.querySelector('#tableResult');
+var upd;
+
+form.addEventListener('submit',(e)=>{
+
+    e.preventDefault();
+
+    if(upd){
+        clearTimeout(upd);
+    }
+
+    const ctype = form.elements.coinType.value;
+
+    fetchPrice(ctype);
+})
+
+const fetchPrice = async(ctype)=>{
+    const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=USD`);
+    const symbol = r.data.coin.symbol;
+    const price = r.data.coin.price;
+    const rank = r.data.coin.rank;
+    const volume = r.data.coin.volume;
+    const change = r.data.coin.priceChange1d;
+    const base = r.data.coin.name;
+    const target = 'USD';
+
+    res.innerHTML = `<tr style ="background-color:blue;color:white;font-weight:700">
+    <td>
+        Property
+    </td>
+    <td>
+        value
+    </td>
+</tr>
+<tr>
+    <td>
+        Symbol
+    </td>
+    <td>
+        ${symbol}
+    </td>
+</tr>
+<tr>
+    <td>
+        ${base}
+    </td>
+    <td>${price} ${target}</td>
+</tr>
+<tr>
+    <td>
+        Rank
+    </td>
+    <td>${rank}</td>
+</tr>
+<tr>
+        <td>
+            Volume(24 hours)
+        </td>
+        <td>${volume}</td>
+</tr>
+<tr>
+        <td>
+            Change(24 hours)
+        </td>
+        <td>${change}</td>
+</tr>`
+
+upd = setTimeout(()=>fetchPrice(ctype),10000);
+    
+}
